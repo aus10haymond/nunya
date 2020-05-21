f81cde9e991020a364204ec7673376230e78467b
Austen Haymond <ahaymon17@gmail.com>
2020-05-21 01:42:29 -0700

    title is link to homepage

--- client/src/pages/components/title.js
+++ client/src/pages/components/title.js
@@ -5,12 +5,14 @@ import "../../assets/styles/title.css"
 function Title() {
     return (
         <div id="titlebar">
+            <a href="/">
             <nav className="navbar justify-content-center">
                 <h1 id="title">NXT LVL</h1>
             </nav>
             <nav className="navbar justify-content-center">
                 <h5 id="tagline">take your business to the next level</h5>
             </nav>
+            </a>
         </div>
     );
 }
