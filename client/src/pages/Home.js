81897392dff7f288432b28c4cfa2ec470f1221bb
Austen Haymond <ahaymon17@gmail.com>
2020-06-19 10:44:05 -0700

    added team section and put all components on to one page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Title from "./components/title";
 import NavBar from "./components/navBar";
@@ -8,28 +7,30 @@ import Projects from "./projects";
 import Start from "./start";
 import Contact from "./contact";
 import Footer from "./components/footer";
+import Team from "./team";
 
 import "../assets/styles/home.css"
 
 function Home() {
     return (
-        <BrowserRouter>
         <div id="home">
             <Title />
             <NavBar />
             <br />
             <div className="container">
-                <Switch>                    
-                    <Route exact path="/" component={Start} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/contact" component={Contact} />
-                </Switch>
+                < Start />
+                <br />
+                <About />
+                <br />
+                <Team />
+                <br />
+                <Projects />
+                <br />
+                <Contact />
             </div>
             <br />
             <Footer />
         </div>
-        </BrowserRouter>
     );
 }
 
---
+++ client/src/pages/team.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import "../assets/styles/team.css"
+
+function Team() {
+    return (
+        <div id="team" className="container">
+            <div className="justify-content-center row">
+                <h2 id="teamtitle">MEET THE TEAM</h2>
+            </div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card" id="teamcard">
+                        <br />
+                        <div className="card-body" id="cardbody">
+                            <img id="teamimg" src="" alt="" />
+                            <h5 className="card-title text-center" id="cardtitle">ANDREA</h5>
+                            <p id="teamcontent">
+
+                            </p>
+                        </div>
+                    </div>
+                    <br />
+                </div>
+                <div className="col-sm-6">
+                <div className="card" id="teamcard">
+                        <br />
+                        <div className="card-body" id="cardbody">
+                            <img id="teamimg" src="" alt="" />
+                            <h5 className="card-title text-center" id="cardtitle">AUSTEN</h5>
+                            <p id="teamcontent">
+
+                            </p>
+                        </div>
+                    </div>
+                    <br />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Team;
