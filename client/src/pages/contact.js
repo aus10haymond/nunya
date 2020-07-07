65ab63a41fd7fa9e923640c38033e48872a47580
Austen Haymond <ahaymon17@gmail.com>
2020-07-06 21:21:48 -0700

    added icons to contact section and instagram and facebook links

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -13,6 +13,7 @@ function Contact() {
                 <div>
                     <div id="contactcontent" className="col-sm-12">
                         <nav id="contact" className="navbar justify-content-center">
+                            <i id="contactlink" class="fas fa-envelope-open-text"></i>
                             <a id="contactlink" className="nav-item nav-link active" href="mailto:nxtlvldgtl@gmail.com?Subject=NXT LVL" target="_top">
                                 <h4>EMAIL</h4>
                             </a>
@@ -21,8 +22,27 @@ function Contact() {
                     <br />
                     <div id="contactcontent" className="col-sm-12">
                         <nav id="contact" className="navbar justify-content-center">
+                            <i id="contactlink" class="fas fa-phone"></i>
                             <a id="contactlink" className="nav-item nav-link active" href="tel:602-301-2522">
                                 <h4>PHONE</h4>
+                            </a>
+                        </nav>
+                    </div>
+                    <br />
+                    <div id="contactcontent" className="col-sm-12">
+                        <nav id="contact" className="navbar justify-content-center">
+                            <i id="contactlink" class="fab fa-instagram"></i>
+                            <a id="contactlink" className="nav-item nav-link active" href="http://instagram.com/_u/nxtlvldgtl/" target="_blank" rel="noopener noreferrer">
+                                <h4>INSTAGRAM</h4>
+                            </a>
+                        </nav>
+                    </div>
+                    <br />
+                    <div id="contactcontent" className="col-sm-12">
+                        <nav id="contact" className="navbar justify-content-center">
+                            <i id="contactlink" class="fab fa-facebook-square"></i>
+                            <a id="contactlink" className="nav-item nav-link active" href="https://www.facebook.com/nxtlvldgtl/" target="_blank" rel="noopener noreferrer">
+                                <h4>FACEBOOK</h4>
                             </a>
                         </nav>
                     </div>
