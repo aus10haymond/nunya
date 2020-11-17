108ea4c84f450142c1fd4b42fc58f9d91d6d957e
Austen Haymond <ahaymon17@gmail.com>
2020-11-16 22:52:55 -0700

    organized downloads folder and changed printables for thanksgiving

--- client/src/pages/components/carousel.js
+++ client/src/pages/components/carousel.js
@@ -2,12 +2,17 @@ import React from "react";
 
 import ImageGallery from "react-image-gallery";
 
-// import booImage from "../../assets/downloads/booImage.png";
-// import hocusPocusImage from "../../assets/downloads/hocusPocusImage.png";
-// import booDownload from "../../assets/downloads/booDownload.pdf";
-// import hocusPocusDownload from "../../assets/downloads/hocusPocusDownload.pdf";
+import chooseToBeGrateful from '../../assets/downloads/thanksgiving/choose-to-be-grateful.png';
+import chooseToBeGratefulDownload from '../../assets/downloads/thanksgiving/choose-to-be-grateful.pdf';
+import leaves from '../../assets/downloads/thanksgiving/leaves.png';
+import gratefulLeaves from '../../assets/downloads/thanksgiving/grateful-leaves.png';
+import gratefulLeavesDownload from '../../assets/downloads/thanksgiving/grateful-leaves.pdf';
+import gratefulStory from '../../assets/downloads/thanksgiving/grateful-story.png';
+import gratefulStoryDownload from '../../assets/downloads/thanksgiving/grateful-story.pdf';
+import gratefulThankfulBlessed from '../../assets/downloads/thanksgiving/grateful-thankful-blessed.png';
+import gratefulThankfulBlessedDownload from '../../assets/downloads/thanksgiving/grateful-thankful-blessed.pdf';
 
-import comingSoon from "../../assets/downloads/comingSoon.png";
+// import comingSoon from "../../assets/downloads/comingSoon.png";
 
 // import "../../assets/styles/image-gallery.css";
 // import "../../assets/styles/carousel.css";
@@ -23,21 +28,36 @@ class Carousel extends React.Component {
     render() {
 
         const images = [
-            // {
-            //     original: booImage,
-            //     description: <a href={booDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Boo!'
-            // },
-            // {
-            //     original: hocusPocusImage,
-            //     description: <a href={hocusPocusDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Hocus Pocus!'
-            // }
             {
-                original: comingSoon,
-                description: 'Coming Soon!',
-                originalAlt: 'Coming Soon!'
+                original: chooseToBeGrateful,
+                description: <a href={chooseToBeGratefulDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Choode to be Grateful'
+            },
+            {
+                original: leaves,
+                description: <a href={gratefulLeavesDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Fall Leaves'
+            },
+            {
+                original: gratefulLeaves,
+                description: <a href={gratefulLeavesDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Leaves quote'
+            },
+            {
+                original: gratefulStory,
+                description: <a href={gratefulStoryDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Grateful Story'
+            },
+            {
+                original: gratefulThankfulBlessed,
+                description: <a href={gratefulThankfulBlessedDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Grateful Thankful Blessed'
             }
+            // {
+            //     original: comingSoon,
+            //     description: 'Coming Soon!',
+            //     originalAlt: 'Coming Soon!'
+            // }
         ]
 
         return (
