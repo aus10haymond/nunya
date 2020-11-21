89938d01f4be76229d1f0efa9b46eaeace00e466
Austen Haymond <ahaymon17@gmail.com>
2020-11-20 17:15:10 -0700

    added new logo and put old logo in footer

--- client/src/pages/components/footer.js
+++ client/src/pages/components/footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-// import "../../assets/styles/footer.css";
+import logoSmall from "../../assets/images/logos/nxt_lvl_logo.png";
 
 function Footer() {
     return (
         <div className="text-center">
            <nav id="footer" className="navbar justify-content-center sticky-bottom">
-                <span className="navbar-text">powered by:</span>
-                <a id="footLink" className="nav-item nav-link active" href="https://aus10haymond.herokuapp.com/" target="_blank" rel="noopener noreferrer">austenhaymond</a>
+               <img id="logoSmall" src={logoSmall} alt="NXT LVL Logo" />
+                <span className="navbar-text">&#169;2020</span>
+                {/* <a id="footLink" className="nav-item nav-link active" href="https://aus10haymond.herokuapp.com/" target="_blank" rel="noopener noreferrer">austenhaymond</a> */}
             </nav>
         </div>
     )
--- client/src/pages/components/title.js
+++ client/src/pages/components/title.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-import logo from "../../assets/images/logos/nxt_lvl_logo.png";
+import logo from "../../assets/images/logos/nld.png";
 // import "../../assets/styles/title.css"
 
 function Title() {
     return (
         <div id="titlebar">
-            {/* <a href="/"> */}
-            <nav className="navbar justify-content-center">
-                <img id="logo" src={logo} alt="NXT LVL Logo" />
-                {/* <h1 id="title">NXT LVL</h1> */}
-            </nav>
-            <nav className="navbar justify-content-center">
-                <h5 id="tagline">take your business to the next level</h5>
-            </nav>
-            {/* </a> */}
+                <nav className="navbar justify-content-center">
+                    <img id="logo" src={logo} alt="NXT LVL Logo" />
+                </nav>
+                <nav className="navbar justify-content-center">
+                    <h5 id="tagline">Take your business to the next level.</h5>
+                </nav>
         </div>
     );
 }
