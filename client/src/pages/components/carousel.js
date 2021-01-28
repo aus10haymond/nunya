d4c1209739a5ab6ace677230c973c5d5e0ad643c
Austen Haymond <ahaymon17@gmail.com>
2021-01-27 20:31:39 -0700

    added valentines day printables

--- client/src/pages/components/carousel.js
+++ client/src/pages/components/carousel.js
@@ -2,47 +2,90 @@ import React from "react";
 
 import ImageGallery from "react-image-gallery";
 
-import comingSoon from "../../assets/downloads/comingSoon.png";
+import bb8 from "../../assets/downloads/valentines/bb8.png";
+import beeMine from "../../assets/downloads/valentines/beeMine.png";
+import chewie from "../../assets/downloads/valentines/chewie.png";
+import conversationHearts from "../../assets/downloads/valentines/conversationHearts.png";
+import friendsLobster from "../../assets/downloads/valentines/friendsLobster.png";
+import grogu from "../../assets/downloads/valentines/grogu.png";
+import hearts from "../../assets/downloads/valentines/hearts.png";
+import heartsGlitter from "../../assets/downloads/valentines/heartsGlitter.png";
+import roses from "../../assets/downloads/valentines/roses.png";
+import smallHearts from "../../assets/downloads/valentines/smallHearts.png";
+import tieFighter from "../../assets/downloads/valentines/tieFighter.png";
+import xoxo from "../../assets/downloads/valentines/xoxo.png";
+
+// import comingSoon from "../../assets/downloads/comingSoon.png";
 class Carousel extends React.Component {
 
     render() {
 
         const images = [
-            // {
-            //     original: beaniePrintable,
-            //     description: <a href={beanieDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Christmas Beanies'
-            // },
-            // {
-            //     original: christmasPrintable,
-            //     description: <a href={christmasDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Christmas lights'
-            // },
-            // {
-            //     original: christmasPrintable2,
-            //     description: <a href={christmasDownload2} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Christmas reindeer'
-            // },
-            // {
-            //     original: christmasTreePrintable,
-            //     description: <a href={christmasTreeDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Christmas Trees'
-            // },
-            // {
-            //     original: christmasTreePrintable2,
-            //     description: <a href={christmasTreeDownload2} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Christmas Trees 2'
-            // },
-            // {
-            //     original: santaHatPrintable,
-            //     description: <a href={santaHatDownload} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
-            //     originalAlt: 'Santa Hats'
-            // }
             {
-                original: comingSoon,
-                description: 'Coming Soon!',
-                originalAlt: 'Coming Soon!'
+                original: bb8,
+                description: <a href={bb8} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'BB-8 Mine'
+            },
+            {
+                original: chewie,
+                description: <a href={chewie} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'ugughhhgh guugh!'
+            },
+            {
+                original: grogu,
+                description: <a href={grogu} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Grogu'
+            },
+            {
+                original: tieFighter,
+                description: <a href={tieFighter} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Sith Valentines'
+            },
+            {
+                original: friendsLobster,
+                description: <a href={friendsLobster} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Pivot!'
+            },
+            {
+                original: beeMine,
+                description: <a href={beeMine} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Bee Mine'
+            },
+            {
+                original: conversationHearts,
+                description: <a href={conversationHearts} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Conversation hearts'
+            },
+            {
+                original: hearts,
+                description: <a href={hearts} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Hearts'
+            },
+            {
+                original: heartsGlitter,
+                description: <a href={heartsGlitter} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Hearts glitter'
+            },
+            {
+                original: smallHearts,
+                description: <a href={smallHearts} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Small hearts'
+            },
+            {
+                original: roses,
+                description: <a href={roses} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'Roses'
+            },
+            {
+                original: xoxo,
+                description: <a href={xoxo} target="_blank" rel="noopener noreferrer">DOWNLOAD</a>,
+                originalAlt: 'XOXO'
             }
+            // {
+            //     original: comingSoon,
+            //     description: 'Coming Soon!',
+            //     originalAlt: 'Coming Soon!'
+            // }
         ]
 
         return (
